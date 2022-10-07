494f87db1584c0cfc05f0557c2776d4cd68f0dbc
Richard Achmatowicz <rachmato@redhat.com>
2022-10-07 16:06:30 -0400

    [WEJBHTTP-81] Add in routing support for processing routes.

---
+++ common/src/main/java/org/wildfly/httpclient/common/RoutingSupport.java
@@ -0,0 +1,29 @@
+package org.wildfly.httpclient.common;
+
+import java.util.Map;
+
+/**
+ * Exposes the mechanism for parsing and formation routing information from/into a requested session identifier.
+ *
+ * @author Paul Ferraro
+ */
+public interface RoutingSupport {
+    /**
+     * Parses the routing information from the specified session identifier.
+     *
+     * @param requestedSessionId the requested session identifier.
+     * @return a map entry containing the session ID and routing information as the key and value, respectively.
+     */
+    Map.Entry<CharSequence, CharSequence> parse(CharSequence requestedSessionId);
+
+    /**
+     * Formats the specified session identifier and route identifier into a single identifier.
+     *
+     * @param sessionId a session identifier
+     * @param route     a route identifier.
+     * @return a single identifier containing the specified session identifier and routing identifier.
+     */
+    CharSequence format(CharSequence sessionId, CharSequence route);
+}
+
+
