26a78db85a0bf21718c4575fb4b823802c59df8f
Richard Opálka <opalka.richard@gmail.com>
2024-05-19 15:09:19 +0200

    Refactoring - There's no need to store tccl in local variable.

--- common/src/main/java/org/wildfly/httpclient/common/HttpTargetContext.java
+++ common/src/main/java/org/wildfly/httpclient/common/HttpTargetContext.java
@@ -165,8 +165,7 @@ public class HttpTargetContext extends AbstractAttachable {
     }
 
     public void sendRequest(ClientRequest request, SSLContext sslContext, AuthenticationConfiguration authenticationConfiguration, HttpMarshaller httpMarshaller, HttpResultHandler httpResultHandler, HttpFailureHandler failureHandler, ContentType expectedResponse, Runnable completedTask, boolean allowNoContent) {
-        final ClassLoader tccl = getContextClassLoader();
-        connectionPool.getConnection(connection -> sendRequestInternal(connection, request, authenticationConfiguration, httpMarshaller, httpResultHandler, failureHandler, expectedResponse, completedTask, allowNoContent, false, sslContext, tccl), failureHandler::handleFailure, false, sslContext);
+        connectionPool.getConnection(connection -> sendRequestInternal(connection, request, authenticationConfiguration, httpMarshaller, httpResultHandler, failureHandler, expectedResponse, completedTask, allowNoContent, false, sslContext, getContextClassLoader()), failureHandler::handleFailure, false, sslContext);
     }
 
     private void sendRequestInternal(final HttpConnectionPool.ConnectionHandle connection, ClientRequest request, AuthenticationConfiguration authenticationConfiguration, HttpMarshaller httpMarshaller, HttpResultHandler httpResultHandler, HttpFailureHandler failureHandler, ContentType expectedResponse, Runnable completedTask, boolean allowNoContent, boolean retry, SSLContext sslContext, ClassLoader classLoader) {
