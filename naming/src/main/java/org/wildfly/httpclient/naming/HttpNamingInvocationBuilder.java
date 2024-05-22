ec29832bdddd365047da33272be6921ab381d58e
Richard Opálka <opalka.richard@gmail.com>
2024-05-22 15:42:04 +0200

    [WEJBHTTP-141] Implementing HttpNamingInvocationBuilder's setRequestPath() method.

--- naming/src/main/java/org/wildfly/httpclient/naming/HttpNamingInvocationBuilder.java
+++ naming/src/main/java/org/wildfly/httpclient/naming/HttpNamingInvocationBuilder.java
@@ -25,7 +25,21 @@ import static io.undertow.util.Methods.GET;
 import static io.undertow.util.Methods.PATCH;
 import static io.undertow.util.Methods.POST;
 import static io.undertow.util.Methods.PUT;
+import static java.net.URLEncoder.encode;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.wildfly.httpclient.common.Protocol.VERSION_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.BIND_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.CREATE_SUBCONTEXT_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.DESTROY_SUBCONTEXT_PATH;
 import static org.wildfly.httpclient.naming.NamingConstants.EXCEPTION;
+import static org.wildfly.httpclient.naming.NamingConstants.LIST_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.LIST_BINDINGS_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.LOOKUP_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.LOOKUP_LINK_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.NAMING_CONTEXT;
+import static org.wildfly.httpclient.naming.NamingConstants.REBIND_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.RENAME_PATH;
+import static org.wildfly.httpclient.naming.NamingConstants.UNBIND_PATH;
 import static org.wildfly.httpclient.naming.NamingConstants.VALUE;
 
 import io.undertow.client.ClientRequest;
@@ -73,16 +87,26 @@ final class HttpNamingInvocationBuilder {
     }
 
     enum InvocationType {
-        BIND,
-        CREATE_SUBCONTEXT,
-        DESTROY_SUBCONTEXT,
-        LIST,
-        LIST_BINDINGS,
-        LOOKUP,
-        LOOKUP_LINK,
-        REBIND_PATH,
-        RENAME_PATH,
-        UNBIND_PATH;
+        BIND(BIND_PATH),
+        CREATE_SUBCONTEXT(CREATE_SUBCONTEXT_PATH),
+        DESTROY_SUBCONTEXT(DESTROY_SUBCONTEXT_PATH),
+        LIST(LIST_PATH),
+        LIST_BINDINGS(LIST_BINDINGS_PATH),
+        LOOKUP(LOOKUP_PATH),
+        LOOKUP_LINK(LOOKUP_LINK_PATH),
+        REBIND(REBIND_PATH),
+        RENAME(RENAME_PATH),
+        UNBIND(UNBIND_PATH);
+
+        private final String path;
+
+        InvocationType(final String path) {
+            this.path = path;
+        }
+
+        String getPath() {
+            return path;
+        }
     }
 
     // helper methods
@@ -103,14 +127,26 @@ final class HttpNamingInvocationBuilder {
         else if (invocationType == InvocationType.LIST_BINDINGS) request.setMethod(GET);
         else if (invocationType == InvocationType.LOOKUP) request.setMethod(POST);
         else if (invocationType == InvocationType.LOOKUP_LINK) request.setMethod(POST);
-        else if (invocationType == InvocationType.REBIND_PATH) request.setMethod(PATCH);
-        else if (invocationType == InvocationType.RENAME_PATH) request.setMethod(PATCH);
-        else if (invocationType == InvocationType.UNBIND_PATH) request.setMethod(DELETE);
+        else if (invocationType == InvocationType.REBIND) request.setMethod(PATCH);
+        else if (invocationType == InvocationType.RENAME) request.setMethod(PATCH);
+        else if (invocationType == InvocationType.UNBIND) request.setMethod(DELETE);
         else throw new IllegalStateException();
     }
 
     private void setRequestPath(final ClientRequest request, final String prefix) {
-        throw new UnsupportedOperationException(); // TODO: implement
+        final StringBuilder sb = new StringBuilder();
+        if (prefix != null) {
+            sb.append(prefix);
+        }
+        appendPath(sb, NAMING_CONTEXT, false);
+        appendPath(sb, VERSION_PATH + version, false);
+        appendPath(sb, invocationType.getPath(), false);
+        appendPath(sb, name.toString(), true);
+        if (newName != null) {
+            sb.append("?new=");
+            sb.append(encode(newName.toString(), UTF_8));
+        }
+        request.setPath(sb.toString());
     }
 
     private void setRequestHeaders(final ClientRequest request) {
@@ -119,6 +155,13 @@ final class HttpNamingInvocationBuilder {
         if (object != null) {
             headers.put(CONTENT_TYPE, VALUE.toString());
         }
+    }
+
+    private static void appendPath(final StringBuilder sb, final String path, final boolean encode) {
+        if (!path.startsWith("/")) {
+            sb.append("/");
+        }
+        sb.append(encode ? encode(path, UTF_8) : path);
     }
 
 }
