c338886c468386cb6d4c4a681f547f8c8a6f6d50
Richard Opálka <opalka.richard@gmail.com>
2024-05-27 15:54:41 +0200

    [WEJBHTTP-140] Adding HTTP request path parameter to RequestType enum.

--- transaction/src/main/java/org/wildfly/httpclient/transaction/RequestType.java
+++ transaction/src/main/java/org/wildfly/httpclient/transaction/RequestType.java
@@ -20,6 +20,15 @@ package org.wildfly.httpclient.transaction;
 
 import static io.undertow.util.Methods.GET;
 import static io.undertow.util.Methods.POST;
+import static org.wildfly.httpclient.transaction.TransactionConstants.UT_BEGIN_PATH;
+import static org.wildfly.httpclient.transaction.TransactionConstants.UT_COMMIT_PATH;
+import static org.wildfly.httpclient.transaction.TransactionConstants.UT_ROLLBACK_PATH;
+import static org.wildfly.httpclient.transaction.TransactionConstants.XA_BC_PATH;
+import static org.wildfly.httpclient.transaction.TransactionConstants.XA_COMMIT_PATH;
+import static org.wildfly.httpclient.transaction.TransactionConstants.XA_FORGET_PATH;
+import static org.wildfly.httpclient.transaction.TransactionConstants.XA_PREP_PATH;
+import static org.wildfly.httpclient.transaction.TransactionConstants.XA_RECOVER_PATH;
+import static org.wildfly.httpclient.transaction.TransactionConstants.XA_ROLLBACK_PATH;
 
 import io.undertow.util.HttpString;
 
@@ -28,24 +37,30 @@ import io.undertow.util.HttpString;
  */
 enum RequestType {
 
-    UT_BEGIN(POST),
-    UT_COMMIT(POST),
-    UT_ROLLBACK(POST),
-    XA_BEFORE_COMPLETION(POST),
-    XA_COMMIT(POST),
-    XA_FORGET(POST),
-    XA_PREPARE(POST),
-    XA_RECOVER(GET),
-    XA_ROLLBACK(POST);
+    UT_BEGIN(POST, UT_BEGIN_PATH),
+    UT_COMMIT(POST, UT_COMMIT_PATH),
+    UT_ROLLBACK(POST, UT_ROLLBACK_PATH),
+    XA_BEFORE_COMPLETION(POST, XA_BC_PATH),
+    XA_COMMIT(POST, XA_COMMIT_PATH),
+    XA_FORGET(POST, XA_FORGET_PATH),
+    XA_PREPARE(POST, XA_PREP_PATH),
+    XA_RECOVER(GET, XA_RECOVER_PATH),
+    XA_ROLLBACK(POST, XA_ROLLBACK_PATH);
 
     private final HttpString method;
+    private final String path;
 
-    RequestType(final HttpString method) {
+    RequestType(final HttpString method, final String path) {
         this.method = method;
+        this.path = path;
     }
 
     HttpString getMethod() {
         return method;
+    }
+
+    String getPath() {
+        return path;
     }
 
 }
