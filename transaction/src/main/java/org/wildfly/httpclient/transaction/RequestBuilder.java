4924bf0e32a81366ac72f6a0240a0923faa250c5
Richard Opálka <opalka.richard@gmail.com>
2024-05-27 13:52:22 +0200

    [WEJBHTTP-140] Introducing RequestBuilder class.

---
+++ transaction/src/main/java/org/wildfly/httpclient/transaction/RequestBuilder.java
@@ -0,0 +1,66 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2024 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.httpclient.transaction;
+
+import io.undertow.client.ClientRequest;
+import org.wildfly.httpclient.common.Protocol;
+
+/**
+ * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
+ */
+final class RequestBuilder {
+
+    private RequestType requestType;
+    private int version = Protocol.LATEST;
+
+    // setters
+
+    RequestBuilder setRequestType(final RequestType requestType) {
+        this.requestType = requestType;
+        return this;
+    }
+
+    RequestBuilder setVersion(final int version) {
+        this.version = version;
+        return this;
+    }
+
+    // helper methods
+
+    ClientRequest createRequest(final String prefix) {
+        final ClientRequest clientRequest = new ClientRequest();
+        setRequestMethod(clientRequest);
+        setRequestPath(clientRequest, prefix);
+        setRequestHeaders(clientRequest);
+        return clientRequest;
+    }
+
+    private void setRequestMethod(final ClientRequest request) {
+        throw new UnsupportedOperationException();
+    }
+
+    private void setRequestPath(final ClientRequest request, final String prefix) {
+        throw new UnsupportedOperationException();
+    }
+
+    private void setRequestHeaders(final ClientRequest request) {
+        throw new UnsupportedOperationException();
+    }
+
+}
