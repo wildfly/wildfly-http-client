81e307e1009b80d163870c963950147894ee7aa8
Richard Opálka <opalka.richard@gmail.com>
2024-05-27 13:26:23 +0200

    [WEJBHTTP-142] Refactoring - Introducing HTTP request path parameter in RequestType enum.

--- ejb/src/main/java/org/wildfly/httpclient/ejb/RequestType.java
+++ ejb/src/main/java/org/wildfly/httpclient/ejb/RequestType.java
@@ -21,6 +21,10 @@ package org.wildfly.httpclient.ejb;
 import static io.undertow.util.Methods.DELETE;
 import static io.undertow.util.Methods.GET;
 import static io.undertow.util.Methods.POST;
+import static org.wildfly.httpclient.ejb.EjbConstants.EJB_CANCEL_PATH;
+import static org.wildfly.httpclient.ejb.EjbConstants.EJB_DISCOVER_PATH;
+import static org.wildfly.httpclient.ejb.EjbConstants.EJB_INVOKE_PATH;
+import static org.wildfly.httpclient.ejb.EjbConstants.EJB_OPEN_PATH;
 
 import io.undertow.util.HttpString;
 
@@ -29,19 +33,25 @@ import io.undertow.util.HttpString;
  */
 enum RequestType {
 
-    START_EJB_INVOCATION(POST),
-    CANCEL_EJB_INVOCATION(DELETE),
-    CREATE_SESSION_EJB(POST),
-    DISCOVER_EJB(GET);
+    START_EJB_INVOCATION(POST, EJB_INVOKE_PATH),
+    CANCEL_EJB_INVOCATION(DELETE, EJB_CANCEL_PATH),
+    CREATE_SESSION_EJB(POST, EJB_OPEN_PATH),
+    DISCOVER_EJB(GET, EJB_DISCOVER_PATH);
 
     private final HttpString method;
+    private final String path;
 
-    RequestType(final HttpString method) {
+    RequestType(final HttpString method, final String path) {
         this.method = method;
+        this.path = path;
     }
 
     HttpString getMethod() {
         return method;
+    }
+
+    String getPath() {
+        return path;
     }
 
 }
