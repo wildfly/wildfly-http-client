2e03945b8623b264316a6234a812a454e05f596c
Richard Opálka <opalka.richard@gmail.com>
2024-09-10 12:58:35 +0200

    [WEJBHTTP-140] Adding Javadoc for RequestType enum.

--- transaction/src/main/java/org/wildfly/httpclient/transaction/RequestType.java
+++ transaction/src/main/java/org/wildfly/httpclient/transaction/RequestType.java
@@ -33,18 +33,80 @@ import static org.wildfly.httpclient.transaction.TransactionConstants.XA_ROLLBAC
 import io.undertow.util.HttpString;
 
 /**
+ * HTTP TXN module invocation types. Each invocation type has {@linkplain #getName() name}, {@linkplain #getMethod() method}
+ * and {@linkplain #getPath() path}. An invocation can be one of the following types:
+ * <ul>
+ *     <li>{@link #UT_BEGIN}<br>
+ *     Establishes a remote user-controlled transaction.
+ *     </li>
+ *     <li>{@link #UT_COMMIT}<br>
+ *     Commits a remote user-controlled transaction.
+ *     </li>
+ *     <li>{@link #UT_ROLLBACK}<br>
+ *     Rolls back a remote user-controlled transaction.
+ *     </li>
+ *     <li>{@link #XA_RECOVER}<br>
+ *     Acquires a list of all unresolved subordinate remote user-controlled transactions.
+ *     </li>
+ *     <li>{@link #XA_BEFORE_COMPLETION}<br>
+ *     Performs before-commit operations, including running all transaction synchronizations on subordinate transaction.
+ *     </li>
+ *     <li>{@link #XA_COMMIT}<br>
+ *     Commits a subordinate transaction.
+ *     </li>
+ *     <li>{@link #XA_FORGET}<br>
+ *     Forgets the (previously prepared) subordinate transaction.
+ *     </li>
+ *     <li>{@link #XA_PREPARE}<br>
+ *     Prepares the subordinate transaction.
+ *     </li>
+ *     <li>{@link #XA_ROLLBACK}<br>
+ *     Rolls back the subordinate transaction.
+ *     </li>
+ * </ul>
+ *
  * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
  */
 enum RequestType {
 
+    /**
+     * {@code UT_BEGIN} invocation type: used to establish a remote user-controlled transaction via HTTP protocol.
+     */
     UT_BEGIN(POST, UT_BEGIN_PATH),
+    /**
+     * {@code UT_COMMIT} invocation type: used to commit remote user-controlled transaction via HTTP protocol.
+     */
     UT_COMMIT(POST, UT_COMMIT_PATH),
+    /**
+     * {@code UT_ROLLBACK} invocation type: used to rollback remote user-controlled transaction via HTTP protocol.
+     */
     UT_ROLLBACK(POST, UT_ROLLBACK_PATH),
-    XA_BEFORE_COMPLETION(POST, XA_BC_PATH),
-    XA_COMMIT(POST, XA_COMMIT_PATH),
-    XA_FORGET(POST, XA_FORGET_PATH),
-    XA_PREPARE(POST, XA_PREP_PATH),
+    /**
+     * {@code XA_RECOVER} invocation type: used to acquire a list of all unresolved subordinate remote user-controlled
+     * transactions from the location associated with this provider via HTTP protocol.
+     */
+    // TODO: THIS IS BUG. The name must be UT_RECOVER & request path must contain 'ut' instead of 'xa' prefix
     XA_RECOVER(GET, XA_RECOVER_PATH),
+    /**
+     * {@code XA_BEFORE_COMPLETION} invocation type: used to perform before-commit operations,
+     * including running all transaction synchronizations on given subordinate transaction via HTTP protocol.
+     */
+    XA_BEFORE_COMPLETION(POST, XA_BC_PATH),
+    /**
+     * {@code XA_COMMIT} invocation type: used to commit the subordinate transaction via HTTP protocol.
+     */
+    XA_COMMIT(POST, XA_COMMIT_PATH),
+    /**
+     * {@code XA_FORGET} invocation type: used to forget the (previously prepared) subordinate transaction via HTTP protocol.
+     */
+    XA_FORGET(POST, XA_FORGET_PATH),
+    /**
+     * {@code XA_PREPARE} invocation type: used to prepare the subordinate transaction via HTTP protocol.
+     */
+    XA_PREPARE(POST, XA_PREP_PATH),
+    /**
+     * {@code XA_ROLLBACK} invocation type: used to roll back the subordinate transaction via HTTP protocol.
+     */
     XA_ROLLBACK(POST, XA_ROLLBACK_PATH);
 
     private final HttpString method;
@@ -55,11 +117,27 @@ enum RequestType {
         this.path = path;
     }
 
-    HttpString getMethod() {
+    /**
+     * Returns the name of this invocation.
+     * @return this invocation {@linkplain #name()}.
+     */
+    final String getName() {
+        return name();
+    }
+
+    /**
+     * Returns the HTTP request method used by this invocation.
+     * @return this invocation HTTP request method.
+     */
+    final HttpString getMethod() {
         return method;
     }
 
-    String getPath() {
+    /**
+     * Returns the HTTP request subpath used by this invocation.
+     * @return this invocation HTTP request subpath.
+     */
+    final String getPath() {
         return path;
     }
 
